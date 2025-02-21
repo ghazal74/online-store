0e9bdcac3417dce1eabaf49970bc4b495ae92b19
ghazal74 <113207449+ghazal74@users.noreply.github.com>
2025-02-21 22:46:10 +0200

    Updated website

--- backend/server.js
+++ backend/server.js
@@ -92,4 +92,4 @@ app.get("/users", async (req, res) => {
 
 // تشغيل الخادم
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(5000, '0.0.0.0', () => console.log(`✅ Server running on port 5000 and accessible from other devices`));
